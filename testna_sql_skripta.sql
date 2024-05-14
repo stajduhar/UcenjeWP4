a328d54e71af2178bcedc258227ed55bc9040e9d
stajduhar <168466382+stajduhar@users.noreply.github.com>
2024-05-14 18:44:37 +0200

    Vjezbanje_GIT_skripta

---
+++ testna_sql_skripta.sql
@@ -0,0 +1,18 @@
+﻿create database testna_baza_vjezbanje_git;
+
+use testna_baza_vjezbanje_git;
+
+create table tablica_1(
+sifra int not null primary key identity(1,1),
+ime varchar(100) not null,
+);
+
+
+create table grupe(
+sifra int not null primary key identity(1,1),
+ime varchar(20) not null,
+prezime varchar(20) not null,
+);
+
+
+
