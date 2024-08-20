1c3da3f5b1daddcf1a7f1ddc9a7133d1cf7c5d97
stajduhar <99borna@gmail.com>
2024-08-20 19:23:15 +0200

    Edukacija 20.08.2024.
    Edukacija 20.08.2024.

---
+++ CSHARP/Ucenje/WebAPI/Controllers/HttpMetodeController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class HttpMetodeController: ControllerBase
+    {
+
+        // ovdje počinje ruta
+
+        [HttpGet]
+        public string Pozdravi()
+        {
+            return "Hello";
+        }
+
+        // ovdje završava ruta
+
+
+
+
+
+        // ovdje počinje ruta
+        [HttpGet]
+        [Route("pozdrav")]
+        public string Pozdravi(string s)
+        {
+            return "Hello " + s;
+        }
+        // ovdje završava ruta
+
+
+        // ovdje počinje ruta
+        [HttpGet]
+        [Route("hello")]
+        public IActionResult Pozdravi(int id, string ime)
+        {
+            return Ok(new { sifra= id, naziv = ime });
+        }
+        // ovdje završava ruta
+
+
+        // ovdje počinje ruta
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return BadRequest("Nešto ne valja");
+        }
+        // ovdje završava ruta
+
+
+        // ovdje počinje ruta
+        [HttpPut]
+        public IActionResult Put(Osoba osoba)
+        {        
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            osoba.Ime = osoba.Ime + " promjenio";
+
+            return StatusCode(StatusCodes.Status202Accepted, osoba);
+
+
+        }
+        // ovdje završava ruta
+
+
+        // ovdje počinje ruta
+        [HttpDelete]
+        public  IActionResult Delete(int id)
+        {
+            return NotFound("Nije pronađeno u bazi");
+
+        }
+        // ovdje završava ruta
+
+
+    }
+}
---
+++ CSHARP/Ucenje/WebAPI/Models/Osoba.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace WebAPI.Models
+{
+    public class Osoba
+    {
+
+        public string? Ime { get; set; }
+
+        public string? Prezime { get; set; }
+
+
+        public DateTime? Date { get; set; }
+
+
+    }
+}
