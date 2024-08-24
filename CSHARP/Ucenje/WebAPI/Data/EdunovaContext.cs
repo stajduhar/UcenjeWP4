87fb266a362671d61ec9a08cab9e5bccda48aaf8
stajduhar <99borna@gmail.com>
2024-08-24 20:42:37 +0200

    Edunova edukacija nadopuna 22.08.2024.
    Edunova edukacija nadopuna 22.08.2024.

---
+++ CSHARP/Ucenje/WebAPI/Data/EdunovaContext.cs
@@ -0,0 +1,15 @@
+﻿using WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Data
+{
+    public class EdunovaContext:DbContext
+    {
+        // ovaj constructor služi da bi se servis od Web Aplikacije povezao na bazu, naš constructor poziva nad constructor od DbContext
+        public EdunovaContext(DbContextOptions<EdunovaContext> opcije): base(opcije) {
+        
+        }
+
+
+    }
+}
--- CSHARP/Ucenje/WebAPI/Program.cs
+++ CSHARP/Ucenje/WebAPI/Program.cs
@@ -1,3 +1,6 @@
+using WebAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// dodavanje baze podataka
+builder.Services.AddDbContext<EdunovaContext>(
+    opcije =>
+    {
+        opcije.UseSqlServer(builder.Configuration.GetConnectionString("EdunovaContext"));
+    }
+    );
+
+
 
 var app = builder.Build();
 
