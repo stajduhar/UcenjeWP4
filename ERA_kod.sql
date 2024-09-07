ce3759d3a427c766039a81bc18d4dffdfa75c9e4
stajduhar <99borna@gmail.com>
2024-09-07 11:12:05 +0200

    promjena naziva tablica
    Tablice se zovu u množini npr sada je books umjesto book

--- ERA_kod.sql
+++ ERA_kod.sql
@@ -4,9 +4,17 @@ drop database if exists audio_library;
 go
 create database audio_library;
 go
-
 use audio_library;
-create table audio_library_account(
+
+
+--select * from books;
+--select * from authors;
+--select * from audio_library_accounts;
+--select * from genres;
+
+
+
+create table audio_library_accounts(
 id int not null primary key identity(1,1),
 username varchar(40) not null,
 first_name varchar(40) not null,
@@ -17,13 +25,13 @@ favorite_genre int not null
 );
 
 
-create table genre(
+create table genres(
 id int not null primary key identity(1,1),
 name_of_genre varchar(40) not null
 );
 
 
-create table book(
+create table books(
 id int not null primary key identity(1,1),
 the_title_of_the_book varchar(20) not null,
 author_of_the_book int not null,
@@ -31,7 +39,7 @@ book_genre int not null
 );
 
 
-create table author(
+create table authors(
 id int not null primary key identity(1,1),
 date_of_birth datetime,
 book_genre int not null,
@@ -41,37 +49,37 @@ nationality varchar(40) not null
 );
 
 
-alter table audio_library_account add foreign key (favorite_genre) references genre(id);
+alter table audio_library_accounts add foreign key (favorite_genre) references genres(id);
 
-alter table book add foreign key (book_genre) references genre(id);
+alter table books add foreign key (book_genre) references genres(id);
 
-alter table author add foreign key (book_genre) references genre(id);
+alter table authors add foreign key (book_genre) references genres(id);
 
-alter table book add foreign key (author_of_the_book) references author(id);
+alter table books add foreign key (author_of_the_book) references authors(id);
 
 
-insert into genre (name_of_genre)
+insert into genres (name_of_genre)
 values 
 ('Triler'),
 ('Mit'),
 ('Realizam');
 
 
-insert into audio_library_account (username,first_name,last_name,date_of_birth,email_account,favorite_genre) 
+insert into audio_library_accounts (username,first_name,last_name,date_of_birth,email_account,favorite_genre) 
 values
 ('iivic123','Ivan','Ivić','1998-05-15','ivanivic98@gmail.com',3),
 ('pperic971','Pero','perić','1993-02-28','peroperic@gmail.com',2),
 ('bboris152','Boris','Bukvić','1991-01-13','borisbukvic1991@gmail.com',1);
 
 
-insert into author (date_of_birth,book_genre,first_name,last_name,nationality)
+insert into authors (date_of_birth,book_genre,first_name,last_name,nationality)
 values
 ('1963-06-21','1','Gosho','Aoyama','Japan'),
 ('1940-05-13','2','Charles Bruce','Chatwin','Engleska'),
 ('1860-01-29','3','Anton','Chekhov','Rusija');
 
 
-insert into book (the_title_of_the_book,author_of_the_book,book_genre)
+insert into books (the_title_of_the_book,author_of_the_book,book_genre)
 values
 ('Detektiv Conan','1','1'),
 ('U Patagoniji','2','2'),
